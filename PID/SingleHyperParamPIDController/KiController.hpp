9cda3d7025ff38ce87c611c4c12e0bca9f0f3f86
HuynhKhuong <thebestman2204@gmail.com>
2023-08-20 11:47:00 +0700

    Add Ki, Kp Controller

---
+++ PID/SingleHyperParamPIDController/KiController.hpp
@@ -0,0 +1,55 @@
+#ifndef __KICONTROLLER_INCLUDED__
+#define __KICONTROLLER_INCLUDED__
+
+namespace P_I_DController
+{
+/// \brief    Implementation of base template class: 1 HyperParam controller 
+/// \details  This implementation version would implement 1 HyperParam controller: Ki
+///           In this controller, the output signal would have integral relationship with input signal: 
+///                             Output = (errorGap + preErrorGap)*Ki*sampleTime/2
+template <typename inputSignalType, typename outputSignalType, typename HyperParamType = float>
+class KiController: public PIDController<inputSignalType, outputSignalType, HyperParamType >
+{
+public: 
+  ///signalType is checked in base class
+  using Base = PIDController<inputSignalType, outputSignalType, HyperParamType>;
+  KiController(HyperParamType _Ki, float _sampleTime): Base(_Ki), sampleTime(_sampleTime){};
+
+  /// \note This function MUST be called after getSystemResponse 
+  ///       getSystemResponse would be designed in higher level to match with different embedded system
+  void calculateErrorGap()
+  {
+    Base::errorGap = static_cast<float>(Base::inputSignal) - static_cast<float>(Base::systemResponse);
+    return;
+  }
+
+  float getOutput() override 
+  {
+    calculateErrorGap();
+
+    if(isControllerDisabled)
+    {
+      Base::calculatedControlOutput = 0.0f;
+    }
+    else 
+    {
+      Base::calculatedControlOutput = Base::hyperParam * sampleTime * static_cast<float>(errorGap + preErrorGap)/2;
+      preErrorGap = errorGap;
+    }
+    return Base::calculatedControlOutput;
+  } 
+
+  void enableController(){isControllerDisabled = false;}
+  void disableController(){isControllerDisabled = true;}
+
+private: 
+  bool isControllerDisabled{false};
+  bool isTriggeredFirstTime{true};
+
+  float sampleTime{0.0f}; 
+  inputSignalType preErrorGap{0U};
+  inputSignalType  errorGap{0U};
+};
+}
+
+#endif 
---
+++ PID/SingleHyperParamPIDController/KpController.hpp
@@ -0,0 +1,43 @@
+#ifndef __KPCONTROLLER_INCLUDED__
+#define __KPCONTROLLER_INCLUDED__
+
+namespace P_I_DController{
+/// \brief    Implementation of base template class: 1 HyperParam controller 
+/// \details  This implementation version would implement 1 HyperParam controller: Kp
+///           In this controller, the output signal would have linear relationship with input signal: 
+///                             Output = Input * Kp
+/// \todo     move getSystemResponse implementation down to lower level as it is hardware specific
+template <typename inputSignalType, typename outputSignalType, typename HyperParamType = float>
+class KpController: public PIDController<inputSignalType, outputSignalType, HyperParamType>
+{
+public: 
+  ///signalType is checked in base class
+
+  using Base = PIDController<inputSignalType, outputSignalType, HyperParamType>;
+  
+  KpController(HyperParamType _Kp): Base(_Kp){};
+
+  float getOutput() override 
+  {
+    if(isControllerDisabled)
+    {
+      Base::calculatedControlOutput = 0.0f;
+    }
+    else 
+    {
+      Base::errorGap = static_cast<float>(Base::inputSignal) - static_cast<float>(Base::systemResponse);
+      Base::calculatedControlOutput = Base::hyperParam * Base::errorGap;
+    }
+
+    return Base::calculatedControlOutput;
+  } 
+ 
+  void enableController(){isControllerDisabled = false;}
+  void disableController(){isControllerDisabled = true;}
+
+private: 
+  bool isControllerDisabled{false};
+};
+} //End of namespace P_I_DController
+
+#endif
